84f22699d7ad3ac413e51abd3e89496e54a6f650
김해동 <hdkim@miridih.com>
2022-08-06 19:43:17 +0900

    #2.2 Movies Service part One

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,3 +1,5 @@
+import { MoviesService } from './movies.service';
+import { Movie } from './entities/movie.entity';
 import {
   Body,
   Controller,
@@ -11,30 +13,27 @@ import {
 
 @Controller('movies')
 export class MoviesController {
+
+  constructor(private readonly moviesService: MoviesService) {}
+
   @Get()
   getAll() {
-    return 'This will return all movies';
-  }
-
-  @Get('search')
-  search(@Query('year') searchingYear: string) {
-    return `We are searching for a movie made after: ${searchingYear}`;
+    return this.moviesService.getAll();
   }
 
   @Get(':id')
-  getOne(@Param('id') movieId: string) {
-    return `This will return one movie with the id: ${movieId}`;
+  getOne(@Param('id') movieId: string): Movie {
+    return this.moviesService.getOne(movieId);
   }
 
   @Post()
   create(@Body() movieData) {
-    console.log(movieData);
-    return movieData;
+    return this.moviesService.create(movieData);
   }
 
   @Delete(':id')
   remove(@Param('id') movieId: string) {
-    return `This will delete a movie with the id: ${movieId}`;
+    return this.moviesService.deleteOne(movieId);
   }
 
   @Patch(':id')
